193d46faf50112032cb24f052c3aa4004b92f23d
Chenxuejiao1399 <48814306+Chenxuejiao1399@users.noreply.github.com>
2020-01-16 17:03:40 +0800

    commit

--- src/views/casesoftong/SicknessHelper.vue
+++ src/views/casesoftong/SicknessHelper.vue
@@ -1,7 +1,10 @@
 <template>
   <van-row class="van_container">
     <vue-headful title="疾病助手"></vue-headful>
-    <van-row class="van_container_body height_auto"> 疾病助手</van-row>
+    <van-row class="van_container_body height_auto">
+      <van-row></van-row>
+      <van-row>疾病助手</van-row>
+    </van-row>
   </van-row>
 </template>
 <script>
